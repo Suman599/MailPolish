ba9572d7add7e0b7652651928074c542e582dc85
Suman Chakraborty <chakrabortysuman599@gmail.com>
2025-08-02 12:37:25 +0530

    Changed a little

--- frontend/src/components/EmailToneImprover.js
+++ frontend/src/components/EmailToneImprover.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
-
+const API_URL = process.env.REACT_APP_API_URL;
 export default function EmailToneImprover() {
   const [emailText, setEmailText] = useState('');
   const [tone, setTone] = useState('formal');
